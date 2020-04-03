5ee5fa668502afc9201965ea068a18208b54faac
ErikBoesen <me@erikboesen.com>
2020-04-03 19:04:17 -0400

    Add slug to link model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import app, db
 
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String)
     redirect_url = db.Column(db.String, required=True)
     title = db.Column(db.String, required=True)
     subtext = db.Column(db.String)
