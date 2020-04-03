3e44541130fa2da8176b672143eec3fb4f261833
ErikBoesen <me@erikboesen.com>
2020-04-03 18:54:15 -0400

    Reorder model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,8 @@ from app import app, db
 
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    redirect_url = db.Column(db.String, required=True)
     title = db.Column(db.String, required=True)
     subtext = db.Column(db.String)
     image_url = db.Column(db.String)
-    redirect_url = db.Column(db.String, required=True)
+    favicon_url = db.Column(db.String)
