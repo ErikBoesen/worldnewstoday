2d7b9faa93874df7193567769f02a7999cfe9c2d
ErikBoesen <me@erikboesen.com>
2020-04-03 19:58:21 -0400

    Stop importing abort

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, abort
+from flask import render_template, redirect, url_for
 from app import app, db
 from app.forms import LinkForm
 from app.models import Link
