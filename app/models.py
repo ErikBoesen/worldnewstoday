5913c844b61edabf5f98f41ab6c0f1a007d1518c
ErikBoesen <me@erikboesen.com>
2020-04-03 20:04:31 -0400

    Use nullable instead of required

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ from app import app, db
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String)
-    redirect_url = db.Column(db.String, required=True)
-    title = db.Column(db.String, required=True)
+    redirect_url = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
     subtext = db.Column(db.String)
     image_url = db.Column(db.String)
     favicon_url = db.Column(db.String)
